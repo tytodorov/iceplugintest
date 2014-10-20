1ab59977e330738e8a3f42b375a64f1b38bfaead
TodorovCeco <tsvetomiry.todorov@telerik.com>
2014-10-20 12:34:11 +0300

    Supress alert if the build is automated

--- www/icetest.js
+++ www/icetest.js
@@ -5,8 +5,10 @@ var utils = require('cordova/utils');
 
 channel.onCordovaReady.subscribe(function() {
 	exec(function(plugin){
-        
+		console.log("Value of the plugin variable is: " + plugin.pluginVariable.toString());
+		if (plugin.pluginVariable.toString().indexOf("AUTOMATION") == -1){
 		utils.alert(plugin.pluginVariable.toString());
+		}
 	}, function(e) {
 		utils.alert("[ERROR] Error initializing Cordova: " + e);
     }, "ICETEST", "getPluginValue", []);
